a26f0034fd6749c96692c4ee1b4bfe5e92a4ecdf
Arya Prast <wardhana.prast@gmail.com>
2015-11-03 14:37:25 +0700

    ubah modul version

--- liquid/liquid.php
+++ liquid/liquid.php
@@ -13,7 +13,7 @@ class Liquid extends Module {
         /**
          * @var string The version of this module
          */
-        private static $version = "2.2.0";
+        private static $version = "1.0.0";
         /**
          * @var string The authors of this module
          */
